9d2382dca5c61025ee9cd6a6fdde8e71b24b4a0f
Xabier de Zuazo <xabier@onddo.com>
2015-05-27 10:52:54 +0200

    version bumped to 0.3.0 WiP

--- metadata.rb
+++ metadata.rb
@@ -24,7 +24,7 @@ maintainer_email 'team@onddo.com'
 license 'Apache 2.0'
 description 'Creates SSH authorized keys files in user home directories.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.0'
+version '0.3.0' # WiP
 
 supports 'amazon'
 supports 'debian'
